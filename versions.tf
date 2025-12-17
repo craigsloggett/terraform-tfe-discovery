728ac84dadb705c3033dd78be0dfbd7c95df7d95
Craig Sloggett <craigsloggett@gmail.com>
2025-12-17 12:01:10 -0500

    docs: Add a comment to justify version constraint

--- versions.tf
+++ versions.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = "~> 1.7"
 
+  # Version 0.71.0 of the `tfe` provider removed service accounts from the
+  # tfe_team_organization_members resource. This module has been written
+  # to match this behaviour when discovering members of the owners team:
+  # https://github.com/hashicorp/terraform-provider-tfe/commit/51c13cc01424fad0d7521022f248c4d3484c0c6f
   required_providers {
     tfe = {
       source  = "hashicorp/tfe"
